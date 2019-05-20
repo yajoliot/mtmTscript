7047392e1a0ad59380bd7fcb42ae2b09cad9b8dc
yan <yan.joli@hotmail.com>
2019-05-20 13:14:43 -0800

    clean up to only include writeTest

--- TransferScript/Program.cs
+++ TransferScript/Program.cs
@@ -28,27 +28,6 @@ using Microsoft.TeamFoundation.WorkItemTracking;
 namespace WordReader
 {
 
-    public struct TestStep
-    {
-
-        public string nLot,
-                      nDocument,
-                      champ,
-                      test,
-                      expected,
-                      result1,
-                      result2;
-
-                      
-    }
-
-    public struct TestCase
-    {
-        public List<TestStep> testSteps;
-    }
-   
-    
-
     
     class Program
     {
@@ -77,102 +56,18 @@ namespace WordReader
         static void Main(string[] args)
         {
 
-            Application word = new Application();
-            Document doc = new Document();
-
-            // Define an object to pass to the API for missing parameters
-            object missing = System.Type.Missing;
-            doc = word.Documents.Open(@"D:\RQemploi\DA0_Devis_RAC_PAL19.docx",
-                    ref missing, ref missing, ref missing, ref missing,
-                    ref missing, ref missing, ref missing, ref missing,
-                    ref missing, ref missing, ref missing, ref missing,
-                    ref missing, ref missing, ref missing);
-
-            List<TestStep> data = new List<TestStep>();
-
-            foreach(Table table in doc.Tables)
-            {
-                for(int i = 1; i <= table.Rows.Count; i++)
-                {
-                    bool isHeader = false;
-                    TestStep temp = new TestStep();
-                    int cellNum = 0;
-                    for(int j = 1; j <= table.Columns.Count; j++)
-                    {
-                        
-                        Cell cell = null;
-                        try
-                        {
-                            cell = table.Cell(i, j);
-                        }
-                        catch (System.Runtime.InteropServices.COMException e)
-                        {
-                            isHeader = true;
-                        }
-                        if (!isHeader)
-                        {
-                            switch (cellNum)
-                            {
-                                case 0:
-                                    temp.nLot = cell.Range.Text;
-                                    break;
-                                case 1:
-                                    temp.nDocument = cell.Range.Text;
-                                    break;
-                                case 2:
-                                    temp.champ = cell.Range.Text;
-                                    break;
-                                case 3:
-                                    temp.test = cell.Range.Text;
-                                    break;
-                                case 4:
-                                    temp.expected = cell.Range.Text;
-                                    break;
-                                case 5:
-                                    temp.result1 = cell.Range.Text;
-                                    break;
-                                case 6:
-                                    temp.result2 = cell.Range.Text;
-                                    break;
-                            }
-                        }
-                        cellNum++;
-                    }
-                    if(!isHeader)
-                        data.Add(temp);
-                }
-            }
-
-            foreach(TestStep ts in data)
-            {
-                Console.WriteLine(ts.nLot);
-                Console.WriteLine(ts.nDocument);
-                Console.WriteLine(ts.champ);
-                Console.WriteLine(ts.test);
-                Console.WriteLine(ts.expected);
-                Console.WriteLine(ts.result1);
-                Console.WriteLine(ts.result2);
-            }
-            
-
-            Console.ReadLine();
-
-            ((_Document)doc).Close();
-            ((_Application)word).Quit();
-
 
             //MTM
 
             string prodName = "TP01";
             string prodYear = "2018-11";
             string testPlanName = $"Liv 2019 - DAO UT {prodName} ({prodYear})";
-            int idTestPlan = 0;
+            int myPlansId = 0;
             string serverurl = "http://gestsource.services.mrq:8080/tfs";
             string project = @"gestsource.services.mrq\RQ\R4-CAB2D-RAC";
             ITestManagementTeamProject proj = GetProject(serverurl, project);
 
             // Get a Test Plan by its Id
-            int myPlansId = idTestPlan;
             ITestPlan plan = proj.TestPlans.Find(myPlansId);
 
 
